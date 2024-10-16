d5e9e778f369047e3c24740b216a9df6c0e7e502
Mehdibayoudh <almehdi.bayoudh@esprit.tn>
2024-10-16 16:37:47 +0100

    back_event

--- resources/views/Back/Event/create.blade.php
+++ resources/views/Back/Event/create.blade.php
@@ -1,19 +1,20 @@
-@extends('layouts.app')
+@extends('layouts_Admin.app')
 
 @section('content')
     <div class="container">
         <section class="page-header">
-            <div class="page-header__bg" style="background-image: url({{ asset('assets/images/backgrounds/page-header-bg-1-1.jpg') }});"></div>
+            <div class="page-header__bg"
+                 style="background-image: url({{ asset('assets/images/backgrounds/page-header-bg-1-1.jpg') }});"></div>
             <!-- /.page-header__bg -->
             <div class="container">
                 <h2>Events</h2>
                 <ul class="thm-breadcrumb list-unstyled">
                     <li><a href="{{ url('/') }}">Home</a></li>
-                    <li>/</li>
-                    <li><span>Edit Event</span></li>
+                    <li>/Create New Event</li>
                 </ul><!-- /.thm-breadcrumb list-unstyled -->
             </div><!-- /.container -->
         </section><!-- /.page-header -->
-        @include('Front.Event.form', ['event' => $event, 'route' => route('events.update', $event->id)])
+
+        @include('Back.Event.form', ['event' => new \App\Models\Event, 'route' => route('eventadmin.store')])
     </div>
 @endsection
